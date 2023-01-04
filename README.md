e14cc09716377a96f0ae1741dcf101d10d027eac
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2023-01-04 21:05:42 +0100

    feat: add docs

--- README.md
+++ README.md
@@ -158,6 +158,8 @@ Coming from `react-use`? Check out our
 
 - #### Miscellaneous
 
+  - [**`useMemoRef`**](https://react-hookz.github.io/web/?path=/docs/miscellaneous-useMemoRef--example)
+    — Like useMemo but with stable refference to avoid closure problem.
   - [**`useSyncedRef`**](https://react-hookz.github.io/web/?path=/docs/miscellaneous-usesyncedref--example)
     — Like `useRef`, but it returns an immutable ref that contains the actual value.
   - [**`useCustomCompareMemo`**](https://react-hookz.github.io/web/?path=/docs/miscellaneous-useCustomCompareMemo--example)
--- src/useMemoRef/useMemoRef.ts
+++ src/useMemoRef/useMemoRef.ts
@@ -2,10 +2,17 @@ import { useMemo, useRef } from 'react';
 
 import type { DependencyList } from 'react';
 
+/**
+ * Like useMemo but with stable refference to avoid closure problem.
+ *
+ * @param factory useMemo factory function
+ * @param deps useMemo dependency list
+ * @returns useMemo result
+ */
 export const useMemoRef = <MemoValue>(factory: () => MemoValue, dependencyList: DependencyList) => {
-  const ref = useRef<MemoValue | null>(null);
   /* eslint-disable react-hooks/exhaustive-deps -- missing factory */
   const memo = useMemo(factory, dependencyList);
+  const ref = useRef<MemoValue>(memo);
 
   ref.current = memo;
 
